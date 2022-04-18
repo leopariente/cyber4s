8697c1c280468e9b7f4d5745ba21ffd3eae23d14
leopariente <96771680+leopariente@users.noreply.github.com>
2022-04-18 14:44:58 +0300

    chamged positioning to js

--- chess.js
+++ chess.js
@@ -5,106 +5,84 @@ document.body.appendChild(chessBoard);
 const chessTable = document.createElement("table");
 chessBoard.appendChild(chessTable);
 
+let selectedCell;
+let pieces = [];
+
+class Piece {
+  constructor(row, col, type, name, movement){
+    this.row = row;
+    this.col = col;
+    this.type = type;
+    this.name = name;
+    this.movement = 0;
+  }
+}
+
+function getInitialBoard() {
+  let result = [];
+  result.push(new Piece(0, 0, "black", "rook"));
+  result.push(new Piece(0, 1, "black", "knight"));
+  result.push(new Piece(0, 2, "black", "bishop"));
+  result.push(new Piece(0, 3, "black", "queen"));
+  result.push(new Piece(0, 4, "black", "king"));
+  result.push(new Piece(0, 5, "black", "bishop"));
+  result.push(new Piece(0, 6, "black", "knight"));
+  result.push(new Piece(0, 7, "black", "rook"));
+  for (let i = 0; i < 8; i++) {
+    result.push(new Piece(1, i, "black", "pawn"));
+  }
+  result.push(new Piece(7, 0, "white", "rook"));
+  result.push(new Piece(7, 1, "white", "knight"));
+  result.push(new Piece(7, 2, "white", "bishop"));
+  result.push(new Piece(7, 3, "white", "queen"));
+  result.push(new Piece(7, 4, "white", "king"));
+  result.push(new Piece(7, 5, "white", "bishop"));
+  result.push(new Piece(7, 6, "white", "knight"));
+  result.push(new Piece(7, 7, "white", "rook"));
+  for (let i = 0; i < 8; i++) {
+    result.push(new Piece(6, i, "white", "pawn"));
+  }
+  return result;
+}
+
+function addImage(cell, type, name) {
+  const image = document.createElement('img');
+  image.src = 'static/' + type + '/' + name + '.png';
+  cell.appendChild(image);
+}
+
+function onCellClick(event) {
+  if (selectedCell !== undefined) {
+    selectedCell.classList.remove('clicked');
+  }
+  selectedCell = event.currentTarget;
+  selectedCell.classList.add('clicked');
+}
+
+function getPlayer(src, cell) {
+  let list = src.split('/');
+  let selectedPlayer;
+  playerType = list[4];
+  playerName = list[5].split('.')[0];
+  return playerName;
+}
+
 for(let i=0; i<8; i++) {
   let chessRow = chessTable.insertRow(i);
     for(let j=0; j<8; j++) {
       let chessTile = chessRow.insertCell(j);
+      chessTile.id = "cell-" + i.toString() + "_" + j.toString();
       if((i+j) % 2 == 0) {
         chessTile.classList.add("light");
       }
       else {
         chessTile.classList.add("dark");
       }
+      chessTile.addEventListener("click", onCellClick)
     }
   }
 
-document.querySelectorAll('td')
-.forEach(tile => tile.addEventListener("click", function() {
-   clickedTile = document.querySelectorAll("td.clicked");
-   if (clickedTile.length == 1) {
-   clickedTile[0].classList.remove("clicked");
-   }
-   tile.classList.add("clicked");
-}));
-
-let blackKing = document.createElement("img");
-blackKing.src = "static/black-king.png"
-
-let blackQueen = document.createElement("img");
-blackQueen.src = "static/black-queen.png"
-
-let blackRook1 = document.createElement("img");
-blackRook1.src = "static/black-rook.png"
-
-let blackRook2 = document.createElement("img");
-blackRook2.src = "static/black-rook.png"
-
-let blackKnight1 = document.createElement("img");
-blackKnight1.src = "static/black-knight.png"
-
-let blackKnight2 = document.createElement("img");
-blackKnight2.src = "static/black-knight.png"
-
-let blackBishop1 = document.createElement("img");
-blackBishop1.src = "static/black-bishop.png"
-
-let blackBishop2 = document.createElement("img");
-blackBishop2.src = "static/black-bishop.png"
-
-let tiles = document.getElementsByTagName("td");
-console.log(tiles)
-
-tiles[0].appendChild(blackRook1);
-tiles[1].appendChild(blackKnight1);
-tiles[2].appendChild(blackBishop1);
-tiles[3].appendChild(blackQueen);
-tiles[4].appendChild(blackKing);
-tiles[5].appendChild(blackBishop2);
-tiles[6].appendChild(blackKnight2);
-tiles[7].appendChild(blackRook2);
-
-for (let i=8; i < 16; i++) {
-  let blackPawn = document.createElement("img");
-  blackPawn.src = "static/black-pawn.png";
-  tiles[i].appendChild(blackPawn);
+pieces = getInitialBoard();
+for (let piece of pieces) {
+  addImage(chessTable.rows[piece.row].cells[piece.col], piece.type, piece.name);
 }
-
-let whiteKing = document.createElement("img");
-whiteKing.src = "static/white-king.png"
-
-let whiteQueen = document.createElement("img");
-whiteQueen.src = "static/white-queen.png"
-
-let whiteRook1 = document.createElement("img");
-whiteRook1.src = "static/white-rook.png"
-
-let whiteRook2 = document.createElement("img");
-whiteRook2.src = "static/white-rook.png"
-
-let whiteKnight1 = document.createElement("img");
-whiteKnight1.src = "static/white-knight.png"
-
-let whiteKnight2 = document.createElement("img");
-whiteKnight2.src = "static/white-knight.png"
-
-let whiteBishop1 = document.createElement("img");
-whiteBishop1.src = "static/white-bishop.png"
-
-let whiteBishop2 = document.createElement("img");
-whiteBishop2.src = "static/white-bishop.png"
-
-tiles[56].appendChild(whiteRook1);
-tiles[57].appendChild(whiteKnight1);
-tiles[58].appendChild(whiteBishop1);
-tiles[59].appendChild(whiteQueen);
-tiles[60].appendChild(whiteKing);
-tiles[61].appendChild(whiteBishop2);
-tiles[62].appendChild(whiteKnight2);
-tiles[63].appendChild(whiteRook2);
-
-for (let i=48; i < 56; i++) {
-  let whitePawn = document.createElement("img");
-  whitePawn.src = "static/white-pawn.png";
-  tiles[i].appendChild(whitePawn);
-}
-
