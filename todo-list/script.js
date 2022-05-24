53617d9f152331ff5cda15261608b1707454ad66
leopariente <leopariente@gmail.com>
2022-05-24 22:19:11 +0300

    added delete functionality

--- todo-list/script.js
+++ todo-list/script.js
@@ -1,24 +1,51 @@
+const addNewTasks = document.createElement("div");
+const inputTask = document.createElement("input");
+inputTask.placeholder = "Add your new todo";
+const addTaskButton = document.createElement("button");
+const taskList = document.createElement("ul");
+
 function loadPage() {
-    const addNewTasks = document.createElement("div");
     document.body.appendChild(addNewTasks);
-    const inputTask = document.createElement("input");
-    const addTaskButton = document.createElement("button");
     addTaskButton.textContent = "+";
     addNewTasks.appendChild(inputTask);
     addNewTasks.appendChild(addTaskButton);
-    const taskList = document.createElement("ul");
     document.body.appendChild(taskList);
 
     addTaskButton.addEventListener("click", () => addTask(inputTask.value));
 }
 
+function deleteTask(todoItem) {
+    todoItem.remove();
+    console.log(taskToDelete);
+}
+
+function editTask(event) {
+
+}
+
 function addTask(task) {
-    console.log(task);
+    inputTask.value = "";
  if (task === "") {
      alert("please write something before creating a task!")
  }
  else {
-     const todoItem = document.createElement("li");
+     const todoItem = document.createElement("div");
+     const todoValue = document.createElement("li");
+     todoValue.textContent = task;
+
+     const deleteButton = document.createElement("button");
+     const editButton = document.createElement("button");
+
+     deleteButton.textContent = "Delete";
+     editButton.textContent = "Edit";
+
+     deleteButton.addEventListener("click", () => deleteTask(todoItem));
+     editButton.addEventListener("click", editTask);
+
+     taskList.appendChild(todoItem);
+     todoItem.appendChild(todoValue);
+     todoItem.appendChild(editButton);
+     todoItem.appendChild(deleteButton);
  }
 }
 
